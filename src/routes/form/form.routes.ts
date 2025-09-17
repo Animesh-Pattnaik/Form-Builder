6e353f18c944f1088e1ecb39753bd08faf8cba05
Devansh <devanshbansal60@gmail.com>
2025-09-17 19:22:45 +0530

    Form Controller and middleware implemented

---
+++ src/routes/form/form.routes.ts
@@ -0,0 +1,35 @@
+import { Router } from "express";
+const formRouter = Router();
+
+import {
+  createNewForm,
+  deleteForm,
+  getAllForm,
+  getFormById,
+  getFormByUrl,
+  getFormStats,
+  publishForm,
+  submitForm,
+  updateForm,
+  GetFormWithSubmissions
+} from "../../controller/form/form.controller";
+import { adminCoordinatorAuthValidation } from "../../middleware/auth/adminCordinator.auth";
+
+formRouter.post("/create", adminCoordinatorAuthValidation, createNewForm);
+
+formRouter.get("/getform/:userId/:formId", getFormById);
+formRouter.get("/getFormWithSubmissions/:formId",adminCoordinatorAuthValidation, GetFormWithSubmissions);
+formRouter.put("/updateForm", updateForm);
+
+formRouter.delete(
+  "/delete/:formid",
+  adminCoordinatorAuthValidation,
+  deleteForm
+);
+formRouter.get("/stats/:userId", getFormStats);
+formRouter.get("/all/:userId", getAllForm);
+formRouter.put("/publish",publishForm);
+formRouter.put("/getformbyurl/:formUrl",getFormByUrl)
+formRouter.put("/submit",submitForm)
+
+export default formRouter;
